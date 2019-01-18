499790ff69768c1003364298b275b45dabcbf72a
Siddarth R <sikanted@microsoft.com>
2019-01-18 16:06:59 +0530

    react-select added

--- src/Utils.ts
+++ src/Utils.ts
@@ -18,4 +18,44 @@ export namespace Utils {
         const url = new URL(urlString);
         return url.host; // localhost:8080
     }
+
+    export function generateUrlParameters(urlParameters: {}): string {
+        const paraExpressions: string[] = [];
+        Object.keys(urlParameters).forEach(key => paraExpressions.push(`${key}=${urlParameters[key]}`));
+        return paraExpressions.join("&");
+    }
+
+    /**
+     * 
+     * @param requestType can be "POST" or "GET"
+     * @param baseUrl Example https://travel.paytm.com/api/flights/v1/airports/c
+     * @param urlParameters Object which contains the parameters to be transfered, Example version=2&international=true&client=web
+     */
+    export function sendRequest(
+        requestType: string,
+        baseUrl: string,
+        urlParameters: {}
+    ): Promise<any> {
+        const generatedQueryParams = urlParameters ? "?" + generateUrlParameters(urlParameters) : "";
+        const url = new URL(baseUrl + generatedQueryParams);
+        return new Promise((resolve, reject) => {
+            const xmlhttp = new XMLHttpRequest();
+            xmlhttp.open(requestType, url.toString());
+            xmlhttp.onload = () => {
+                if (xmlhttp.status >= 200 && xmlhttp.status < 303) {
+                    const returnedAjaxData = JSON.parse(xmlhttp.responseText);
+                    resolve(returnedAjaxData);
+                } else {
+                    reject({ responseCode: xmlhttp.status, message: xmlhttp.responseText });
+                }
+            };
+
+            xmlhttp.onerror = () => reject({ responseCode: xmlhttp.status, message: xmlhttp.responseText });
+            xmlhttp.ontimeout = () => reject({ responseCode: xmlhttp.status, message: xmlhttp.responseText });
+            // Content-Type,Cache-Control,Authorization,auth
+            xmlhttp.setRequestHeader("Accept", "application/json");
+            xmlhttp.setRequestHeader("Content-Type", "application/json");
+            xmlhttp.send();
+        });
+    }
 }
