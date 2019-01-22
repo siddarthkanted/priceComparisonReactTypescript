eff1b4172f4f830b8d8ee600db392a4670bdfd62
Siddarth R <sikanted@microsoft.com>
2019-01-22 11:42:06 +0530

    pivot item added in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,55 @@
 import { initializeIcons } from '@uifabric/icons';
+import { Pivot, PivotItem, PivotLinkFormat } from 'office-ui-fabric-react/lib/Pivot';
 import * as React from 'react';
 import { Route, Switch } from "react-router";
 import { GroceryMain } from "src/components/Product/GroceryMain";
 import { Generic } from "src/components/Travel/Generic";
+import { Utils } from "src/Utils";
 
 initializeIcons();
 
+interface IPath {
+  path: string;
+  render: () => JSX.Element
+}
+
 class App extends React.Component<any, any> {
+  private pathList: IPath[] = [
+    { path: "Flight", render: this.renderFlight },
+    { path: "Train", render: this.renderTrain },
+    { path: "Grocery", render: this.renderGrocery }
+  ];
+
   public render() {
     return (
       <Switch>
-        <Route path="/Train" render={(props) => this.renderTrain()} />
-        <Route path="/Flight" render={(props) => this.renderFlight()} />
-        <Route path="/" component={GroceryMain} /> />
-    </Switch>
+        {this.pathList.map(path => this.renderRoute(path))}
+      </Switch>
     );
+  }
+
+  private renderRoute(path: IPath): JSX.Element {
+    return <Route path={"/" + path.path} render={(props) => this.renderPivot(path)} key={path.path} />
+  }
+
+  private renderPivotItem(path: IPath): JSX.Element {
+    return (
+      <PivotItem linkText={path.path} itemKey={path.path} key={path.path}>
+        {path.render()}
+      </PivotItem>
+    );
+  }
+
+  private renderPivot(selectedPath: IPath): JSX.Element {
+    return (
+      <Pivot linkFormat={PivotLinkFormat.tabs} selectedKey={selectedPath.path} onLinkClick={this.onPivotItemClick}>
+        {this.pathList.map(pathItem => this.renderPivotItem(pathItem))}
+      </Pivot>
+    );
+  }
+
+  private renderGrocery(): JSX.Element {
+    return <GroceryMain />
   }
 
   private renderFlight(): JSX.Element {
@@ -32,6 +67,13 @@ class App extends React.Component<any, any> {
         Options={trainOptions}
       />);
   }
+
+  private onPivotItemClick(item: PivotItem): void {
+    if (item.props.itemKey) {
+      window.open(Utils.getUrl(item.props.itemKey),"_self");
+    }
+  }
+
 }
 
 // 0 fromPlace
@@ -50,6 +92,7 @@ const trainLinks = [
 ];
 
 const flightLinks = [
+  "https://www.cleartrip.com/flights/results?from={0}&to={1}&depart_date={2}/{3}/{4}&adults=1&childs=0&infants=0&class=Economy",
   "https://in.via.com/flight/search?returnType=one-way&destination={1}&source={0}&month={3}&day={2}&year={4}&numAdults=1&numChildren=0&numInfants=0",
   "https://flight.easemytrip.com/FlightList/Index?org={0}-{0}&dept={1}-{1}&adt=1&chd=0&inf=0&cabin=0&airline=undefined&deptDT={2}/{3}/{4}&isOneway=true",
   "https://www.happyeasygo.com/flights/{0}-{1}/{4}-{3}-{2}?tripType=0&adults=1&childs=0&baby=0&cabinClass=Economy",
