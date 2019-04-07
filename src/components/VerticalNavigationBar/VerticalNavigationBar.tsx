e34c025e214d026fadac4d157429ef8504a74865
Siddarth R <sikanted@microsoft.com>
2019-04-07 15:42:14 +0530

    vertical navigation sorted and updated

--- src/components/VerticalNavigationBar/VerticalNavigationBar.tsx
+++ src/components/VerticalNavigationBar/VerticalNavigationBar.tsx
@@ -1,4 +1,5 @@
 import { Dictionary } from "lodash";
+import orderBy from "lodash-es/orderBy";
 import { INavLink, Nav } from 'office-ui-fabric-react/lib/Nav';
 import * as React from "react";
 import { Utils } from "src/common/Utils";
@@ -11,9 +12,9 @@ interface IVerticalNavigationBarProps {
 
 export const VerticalNavigationBar: React.SFC<IVerticalNavigationBarProps> = (props: IVerticalNavigationBarProps) => {
     const { dataList } = props;
-    const categoriesDictionary: Dictionary<string[]> = {};
+    const categoriesDictionary: Dictionary<Set<string>> = {};
     dataList.map(data => addCategory(categoriesDictionary, data));
-    const keyArray = Object.keys(categoriesDictionary);
+    const keyArray = orderBy(Object.keys(categoriesDictionary));
     const links = keyArray.map(key => createLinks(categoriesDictionary, key));
     return (
         <Nav groups={[
@@ -27,13 +28,17 @@ export const VerticalNavigationBar: React.SFC<IVerticalNavigationBarProps> = (pr
 /**
  * Create Parent Link
  */
-function createLinks(categoriesDictionary: Dictionary<string[]>, parentCateogry: string): INavLink {
-    const links = categoriesDictionary[parentCateogry].map(childCategory => createLink(parentCateogry, childCategory));
+function createLinks(categoriesDictionary: Dictionary<Set<string>>, parentCateogry: string): INavLink {
+    const childCategories = orderBy(Array.from(categoriesDictionary[parentCateogry]));
+    const links: INavLink[] = [];
+    childCategories.forEach(childCategory => {
+        links.push(createLink(parentCateogry, childCategory));
+    }); 
     return {
         isExpanded: true,
         links,
         name: parentCateogry,
-        onClick: () =>  Utils.setUrlPath("Grocery", parentCateogry),
+        onClick: () => Utils.setUrlPath("Grocery", parentCateogry),
         url: ""
     };
 }
@@ -45,14 +50,14 @@ function createLink(parentCateogry: string, childCategory: string): INavLink {
     return {
         key: childCategory,
         name: childCategory,
-        onClick: () =>  Utils.setUrlPath("Grocery", parentCateogry, childCategory),
+        onClick: () => Utils.setUrlPath("Grocery", parentCateogry, childCategory),
         url: ""
     }
 }
 
-function addCategory(categoriesDictionary: Dictionary<string[]>, data: any): void {
+function addCategory(categoriesDictionary: Dictionary<Set<string>>, data: any): void {
     if (!categoriesDictionary[data.Category[0]]) {
-        categoriesDictionary[data.Category[0]] = [];
+        categoriesDictionary[data.Category[0]] = new Set<string>();
     }
-    categoriesDictionary[data.Category[0]].push(data.Category[1]);
+    categoriesDictionary[data.Category[0]].add(data.Category[1]);
 }
